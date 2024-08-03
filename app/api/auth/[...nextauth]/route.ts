facf5c7bc00a713eb08bbca2531bee4284340489
houhoz <houyazhao@gmail.com>
2024-08-03 17:07:02 +0800

    @auth/prisma-adapter --> @next-auth/prisma-adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,25 @@
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/prisma/client';
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: 'jwt',
+  },
 };
 
 const handler = NextAuth(authOptions);
+
+// 修改版本
+// https://authjs.dev/getting-started/adapters/prisma?_gl=1*1fj34sn*_gcl_au*MjA4NDgxNzg0My4xNzIyNjA5NTE4LjE2NDA3NjAyNTguMTcyMjYxNDIwMS4xNzIyNjE0MjA0
 
 export { handler as GET, handler as POST };

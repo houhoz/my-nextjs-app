f913fba72bfb0585067d04492059791c48512d3e
houhoz <houyazhao@gmail.com>
2024-08-04 10:36:22 +0800

    dynamic HeavyComponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,38 @@
-import Link from 'next/link';
-import ProductCard from './components/ProductCart';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/lib/auth';
+'use client';
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+import Link from 'next/link';
+// import { getServerSession } from 'next-auth';
+// import { authOptions } from '@/app/lib/auth';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+const HeavyComponent = dynamic(
+  () => import('@/app/components/HeavyComponent'),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  }
+);
+
+export default function Home() {
+  const [isVisible, setIsVisible] = useState(false);
+  // const session = await getServerSession(authOptions);
   return (
     <main>
-      <h1 className="font-mona">
+      {/* <h1 className="font-mona">
         hello {session && <span>{session.user!.name}</span>}
-      </h1>
+      </h1> */}
       <Link href="/users">Users</Link>
-      <ProductCard />
+      <div>
+        <button
+          className="btn"
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          {isVisible ? 'Hide' : 'Show'}
+        </button>
+      </div>
+
+      {isVisible && <HeavyComponent />}
     </main>
   );
 }
